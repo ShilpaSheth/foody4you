84097b4709bd99c52ab757d90ab52bfafc81f7a4
Sumit Das <sumitdasedu039@gmail.com>
2022-05-15 09:44:05 +0530

    production url added

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const DEVELOPMENT_URL = "http://localhost:5454";
-// const PRODUCTION_URL = "";
+//const DEVELOPMENT_URL = "http://localhost:5454";
+ const PRODUCTION_URL = " https://foody4.herokuapp.com/";
 
 exports.API_URL = DEVELOPMENT_URL;
