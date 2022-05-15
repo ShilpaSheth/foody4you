1d679350ace2f1618f359b81ad56449ae61aa002
Sumit Das <sumitdasedu039@gmail.com>
2022-05-15 09:47:27 +0530

    production issue fixed

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 //const DEVELOPMENT_URL = "http://localhost:5454";
  const PRODUCTION_URL = " https://foody4.herokuapp.com";
 
-exports.API_URL = DEVELOPMENT_URL;
+//exports.API_URL = DEVELOPMENT_URL;
+exports.API_URL = PRODUCTION_URL;
